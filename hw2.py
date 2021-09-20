13c2e59cf26da5f4a880b1ce7f057fa9f76fbb6c
Mihail <41691809+Koallla@users.noreply.github.com>
2021-09-20 21:38:28 +0300

    added hw2.1

---
+++ hw2.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+url = 'https://uk.m.wikipedia.org/wiki/%D0%9D%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8F_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B8#%D0%9D%D0%B0%D1%80%D0%BE%D0%B4%D0%B6%D1%83%D0%B2%D0%B0%D0%BD%D1%96%D1%81%D1%82%D1%8C'
+match = 'Коефіцієнт народжуваності в регіонах України'
+
+def writer(data):
+    with open('data.json', 'w', encoding='utf-8') as file:
+        data.to_json(file, orient="columns")
+
+
+try:
+    df = pd.read_json('data.json', orient='values')
+except ValueError:
+    df = pd.read_html(url, match=match)[0]
+    writer(df)
+    df = pd.read_json('data.json', orient='values')
+
+
+head_data = df.head()
+shape = df.shape
+df.replace("—", np.nan, inplace=True)
+dtypes = df.dtypes
+df = df.astype({'2014': np.float64, '2019': np.float64})
+col_isnull_sum = df.isnull().sum()
+df.drop([27], inplace=True)
+df.fillna(df.mean(numeric_only=True), inplace=True)
+df[df['2019'] > df['2019'].mean()]['Регіон']
+max_val_2014 = df.loc[df['2014'] == df['2014'].max()]['Регіон']
+df[['Регіон', '2019']].plot.bar(rot=0)
+plt.show()
