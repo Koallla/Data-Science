81ce027bb0e9f63ce9beab6e9b4b73cc05059ad9
Mihail <41691809+Koallla@users.noreply.github.com>
2021-09-21 21:39:16 +0300

    fixed read_html

--- hw2.2.py
+++ hw2.2.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 # url = 'https://drive.google.com/file/d/1JMYqXipZpz9Y5-vyxvLEO2Y1sRBxqu-U/view'
 url = '2017_jun_final.csv'
+
 def del_col_with_null(df_data):
     for col in df_data:
         if df_data[col].isnull().sum() and col != 'Язык.программирования':
             df.drop(col, axis=1, inplace=True)
 
-        
+
+def fill_avg_salary(df_data):
+    return df_data.mean()
 
 
 df = pd.read_csv(url)
@@ -20,5 +23,12 @@ df = df.dropna()
 df.shape
 python_data = df[df['Язык.программирования'] == 'Python']
 python_data.shape
-grouped = python_data.groupby(by='Должность').head(207)
-print(grouped)
+grouped = python_data.groupby(by='Должность')
+min_max_salary = grouped[['Зарплата.в.месяц']].agg([
+    lambda x: x.min(),
+    lambda x: x.max()
+])
+
+min_max_salary['avg'] = min_max_salary.apply(fill_avg_salary, axis=1)
+desc_avg = min_max_salary['avg'].describe()
+desc_avg.to_csv('data_salary.csv')
--- hw2.py
+++ hw2.py
@@ -7,20 +7,24 @@ import matplotlib.pyplot as plt
 url = 'https://uk.m.wikipedia.org/wiki/%D0%9D%D0%B0%D1%81%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8F_%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B8#%D0%9D%D0%B0%D1%80%D0%BE%D0%B4%D0%B6%D1%83%D0%B2%D0%B0%D0%BD%D1%96%D1%81%D1%82%D1%8C'
 match = 'Коефіцієнт народжуваності в регіонах України'
 
-def writer(data):
-    with open('data.json', 'w', encoding='utf-8') as file:
-        data.to_json(file, orient="columns")
+# def writer(data):
+#     with open('data.json', 'w', encoding='utf-8') as file:
+#         data.to_json(file, orient="columns")
 
 
-try:
-    df = pd.read_json('data.json', orient='values')
-except ValueError:
-    df = pd.read_html(url, match=match)[0]
-    writer(df)
-    df = pd.read_json('data.json', orient='values')
+# try:
+#     df = pd.read_json('data.json', orient='values')
+# except ValueError:
+#     df = pd.read_html(url, match=match)[0]
+#     writer(df)
+#     df = pd.read_json('data.json', orient='values')
 
+
+
+df = pd.read_html(url, match=match, thousands='.', decimal=',')[0]
 
 head_data = df.head()
+print(head_data)
 shape = df.shape
 df.replace("—", np.nan, inplace=True)
 dtypes = df.dtypes
